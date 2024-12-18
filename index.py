f234dd8b015981e8d51e2daf3842921cd8cbd350
Deibson Cogo <deibsoncogo@outlook.com>
2024-12-18 20:01:00 -0300

    feat: criação do menu

---
+++ index.py
@@ -0,0 +1,10 @@
+while True:
+  print(" BEM VINDO A AGENDA DE CONTATOS")
+  print("  1. ADICIONAR UM CONTATO")
+  print("  2. LISTAR OS CONTATOS")
+  print("  3. EDITAR UM CONTATO")
+  print("  4. EXCLUIR UM CONTATO")
+  print("  5. FAVORITOS")
+  print("  6. SAIR")
+
+  option = int(input("\n  ESCOLHA UMA OPÇÃO: "))
